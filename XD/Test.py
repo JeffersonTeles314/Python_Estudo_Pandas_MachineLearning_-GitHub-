54677afc97998387200b2aa72b6569b239a46e7b
Jefferson Teles <jeffersonteles007@gmail.com>
2022-08-22 18:27:39 -0500

    commit

---
+++ XD/Test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Salvando uma O Caminho do Arquivo em Uma Variável Para Lhor Acesso
+plantest_caminho_arqv = 'plantest.csv'
+# Lendo os Dados e armazenando os Dados em um DataFrame Chamado melbourne_data
+plantest_data = pd.read_csv(plantest_caminho_arqv) 
+# Imprimindo um Sumário dos Dados
+print(plantest_data.describe())
