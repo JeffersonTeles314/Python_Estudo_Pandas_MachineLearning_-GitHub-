1d7c27c0f19e14b34823f65512905f692b9b6423
Jefferson Teles <jeffersonteles007@gmail.com>
2022-08-22 18:28:49 -0500

    Update Test.py

--- XD/Test.py
+++ XD/Test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Salvando uma O Caminho do Arquivo em Uma Variável Para Lhor Acesso
-plantest_caminho_arqv = 'plantest.csv'
+plantest_caminho_arqv = 'XD/plantest.csv'
 # Lendo os Dados e armazenando os Dados em um DataFrame Chamado melbourne_data
 plantest_data = pd.read_csv(plantest_caminho_arqv) 
 # Imprimindo um Sumário dos Dados
