411f16e589fe2cf5b65f557224952626384543c2
Jefferson Teles <jeffersonteles007@gmail.com>
2022-08-23 10:32:38 -0500

    Commit

--- Pandas/Test.py
+++ Pandas/Test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Salvando uma O Caminho do Arquivo em Uma Variável Para Lhor Acesso
-melbourne_caminho_arqv = '../input/melbourne-housing-snapshot/melb_data.csv'
+plantest_caminho_arqv = 'Pandas/plantest.csv'
 # Lendo os Dados e armazenando os Dados em um DataFrame Chamado melbourne_data
-melbourne_data = pd.read_csv(melbourne_file_path) 
+plantest_data = pd.read_csv(plantest_caminho_arqv) 
 # Imprimindo um Sumário dos Dados
-melbourne_data.describe()
+print(plantest_data.describe())
