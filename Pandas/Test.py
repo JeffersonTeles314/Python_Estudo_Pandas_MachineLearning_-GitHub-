45a81a9080ffbdb43d252a692e7ea842c3345c70
Jefferson Teles <jeffersonteles007@gmail.com>
2022-08-25 15:25:35 -0500

    Update Test.py

--- Pandas/Test.py
+++ Pandas/Test.py
@@ -1,8 +1,14 @@
+
+#Parte 1.0 - Importando a Biblioteca
 import pandas as pd
 
-# Salvando uma O Caminho do Arquivo em Uma Variável Para Lhor Acesso
+# Parte 2.0 - Salvando uma O Caminho do Arquivo em Uma Variável Para Lhor Acesso
 plantest_caminho_arqv = 'Pandas/plantest.csv'
-# Lendo os Dados e armazenando os Dados em um DataFrame Chamado melbourne_data
+
+# Parte 3.0 - Criando um Dataframe
+# Parte 3.1 - Lendo os Dados de um "csv", e armazenando em um DataFrame.
 plantest_data = pd.read_csv(plantest_caminho_arqv) 
+
 # Imprimindo um Sumário dos Dados
 print(plantest_data.describe())
+
