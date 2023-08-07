761abeceed85b32781c1fbe91926f6c680fc6de0
JeffersonTeles314 <jeffersonteles007@gmail.com>
2023-08-07 09:17:35 -0500

    xcvxcv

---
+++ Pandas/test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+reviews = pd.read_csv("Pandas/plantest.csv")
+pd.set_option("display.max_rows", 5)
+
+#print(pd.DataFrame(reviews.groupby('Valor_A')).Valor_A)
+pd.to_string(reviews.groupby('Valor_A').Valor_B)
