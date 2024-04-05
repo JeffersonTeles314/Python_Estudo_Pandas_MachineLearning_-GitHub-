a8f2741d7fe6894bc5fa918ea7e1633d35fefd37
jefferson.almeida <jeffersonteles007@gmail.com>
2024-04-05 11:39:11 -0500

    Update test.py

--- Pandas/test.py
+++ Pandas/test.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 reviews = pd.read_csv("Pandas/plantest.csv")
-pd.set_option("display.max_rows", 5)
+print(reviews.Valor_A)
+
+
+# pd.set_option("display.max_rows", 5)
 
 #print(pd.DataFrame(reviews.groupby('Valor_A')).Valor_A)
-pd.to_string(reviews.groupby('Valor_A').Valor_B)
+#pd.to_string(reviews.groupby('Valor_A').Valor_B)
